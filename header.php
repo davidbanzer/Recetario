9ba4b1623b88b6c6a8db0ea2a93b884a74d355c4
David <davidbanzer123@gmail.com>
2021-03-24 10:55:06 -0400

    Header Listo 2

--- header.php
+++ header.php
@@ -9,12 +9,13 @@
     <script src="vendor/components/jquery/jquery.js" type="text/javascript"></script>
     <link href="vendor/twbs/bootstrap/dist/css/bootstrap.css" rel="stylesheet" />
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"> <i class="fas fa-hamburger mr-2"></i>Recetas Fáciles</a>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
+        <a class="navbar-brand" href="#"> <i class="fas fa-hamburger mr-2"></i>Recetas Facilitas</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -22,13 +23,17 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#"><i class="fas fa-hat-chef mr-2"></i>Inicio <span
+                            class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Admin <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#"><i class="fas fa-hat-chef mr-2"></i>Admin <span
+                            class="sr-only">(current)</span></a>
                 </li>
             </ul>
         </div>
+        <nav class="navbar navbar-light bg-light">
+        </nav>
     </nav>
 </body>
 
